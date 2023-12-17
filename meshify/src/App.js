e61432bae30f33dff30a9d26cdaec6fc80f0bd09
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2023-12-17 09:50:04 +0530

    Modified the Server
    Signed-off-by: Khushi Kesharwani <khushibharatkesharwani@gmail.com>

--- meshify/src/App.js
+++ meshify/src/App.js
@@ -1,14 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import Provider from './components/Provider/Provider';
-import Dashboard from './components/Dashboard/Dashboard';
+import React from "react";
+import { useState } from 'react';
+import NavigationDrawer from "./Components/NavDrawer/NatvigationDrawer";
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import Dashboard from "../src/pages/Dashboard"
+import Settings from "./pages/Settings";
+import Istio from "./pages/Istio";
+import { disableBodyScroll } from 'body-scroll-lock';
+import Auth from "./pages/Authentication/Auth";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
   return (
-    <div className="App">
-      <Provider />
-      <Dashboard />
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+        <Route
+          path="/"
+          element={isAuthenticated ? <Dashboard /> : <Navigate to="/provider" />}
+          />
+        <Route
+          path="/provider"
+          element={<Auth onLogin={handleLogin} />}
+           />
+        <Route
+          path="/dashboard"
+          element={<Dashboard/>}
+        />
+        <Route path="/istio" element={<Istio/>} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>  
+    </BrowserRouter>
+    </>
   );
 }
 
